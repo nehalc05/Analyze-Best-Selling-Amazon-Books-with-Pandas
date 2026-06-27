ca0093cf92e3f388d768779a2306097dd0d59312
nehalc05 <nch34598@gmail.com>
2026-06-27 17:42:22 +0530

    added abg rating by genre

--- main.py
+++ main.py
@@ -1,6 +1,9 @@
 import pandas as pd
 df=pd.read_csv('bestsellers.csv')
-print(df.head())
-print(df.shape)
-print(df.columns)
-print(df.describe())
+df.drop_duplicates(inplace=True)
+df.rename(columns={"Name": "Title", "Year": "Publication Year", "User Rating": "Rating"}, inplace=True)
+df["Price"]=df["Price"].astype(float)
+author_counts = df['Author'].value_counts()
+print(author_counts)
+avg_rating_by_genre = df.groupby("Genre")["Rating"].mean()
+print(avg_rating_by_genre)
